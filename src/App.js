e962aade6b74d90c1f6d54fcc139dbc5bd11ebe8
Olorunda_Samuel <olorundasamuel2@gmail.com>
2021-03-15 22:16:16 +0100

    Autentication Using Firebase And React Js creacting a collection inside a collection which both has a uniqe ID

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HomeScreen from "./component/HomeSCreen";
+import "antd/dist/antd.css";
+
+function App() {
+  return (
+    <div>
+      <br />
+      <br />
+      <br />
+      <HomeScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/base.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+
+export const app = firebase.initializeApp({
+  apiKey: "AIzaSyD2y5Q3ronX-IhQ3obUCCGX8JoJ2Cxl7xA",
+  authDomain: "ubani-24.firebaseapp.com",
+  projectId: "ubani-24",
+  storageBucket: "ubani-24.appspot.com",
+  messagingSenderId: "254420761353",
+  appId: "1:254420761353:web:1e3a6ae638d14eb12df1d8",
+});
